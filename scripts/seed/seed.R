9a274e81fecb0bab7d015e6a8ce76f30912bc7bc
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2024-09-05 12:02:53 +0000

    seed

---
+++ scripts/seed/seed.R
@@ -0,0 +1,7 @@
+# This contains code for the intitial setup of the webpage
+#  Prior to this
+#   I creates the space tcrenv on the github
+#   Created new project in RStudio using the "Version control"
+
+# Seed a quarto webpage scaffolding
+system("quarto create project website .")
