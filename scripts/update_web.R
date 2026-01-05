e99620bda95c2df4c202412f5054a233127f5082
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2026-01-05 09:11:17 +0000

    sync 2025-01-05

--- scripts/update_web.R
+++ scripts/update_web.R
@@ -1,3 +1,5 @@
 system("quarto preview --render all --no-watch-inputs --no-browse")
 system("cp -r _site/* /net/hafri.hafro.is/export/home/hafri/einarhj/public_html/edu/2025groftp/.")
 system("chmod -R a+rX /net/hafri.hafro.is/export/home/hafri/einarhj/public_html/edu/2025groftp")
+
+system("cp -rp data-raw /net/hafri.hafro.is/export/home/hafri/einarhj/public_html/edu/2025groftp/.")
